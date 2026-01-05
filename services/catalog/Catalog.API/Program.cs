fc86449a3263ee9414319d7f03c560cadc99bdb9
Ziad Rawash <ZiadRawash524@gmail.com>
2026-01-05 04:41:12 +0200

    feat(catalog): add service with API, Application, Infrastructure layers and update swagger metadata

---
+++ services/catalog/Catalog.API/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "This is API for catalog microservice in ecommerce-platform",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Email = "ziadrawash524@gmail.com",
+            Name = "Ziad Rawash",
+            Url = new Uri("https://github.com/ZiadRawash")
+        }
+    });
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
